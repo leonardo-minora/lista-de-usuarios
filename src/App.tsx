efd5dd19ee125ce41aa3ce8a641f5c1e13643afb
Leonardo Minora <leonardo.minora@gmail.com>
2022-07-27 09:26:31 -0300

    Adicionado código para acessar API

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = (props: any) => {
+	const [users, setUsers] = useState([
+		{ id: 1, name: "Minora" },
+		{ id: 2, name: "Ataide" },
+	]);
+  const [loading, setLoading] = useState(true);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+	useEffect(() => {
+		fetch("https://jsonplaceholder.typicode.com/users/")
+			.then((response) => response.json())
+			.then((json) => {setUsers(json); setLoading(false)});
+	});
+	return (
+		<div className="App">
+			<h1>Lista de usuários</h1>
+			<div className="card">
+        {loading ? <h2>Carregando...</h2> : null}
+				<ul>
+					{users.map((user) => (
+						<li key={user.id}>{user.name}</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
+};
 
-export default App
+export default App;
